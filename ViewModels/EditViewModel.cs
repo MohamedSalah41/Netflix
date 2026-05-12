fd3ec7f10375be2666764017f8b67d9e202cb20c
mohamedkhaledmk <khaled25256@gmail.com>
2026-05-12 14:49:19 +0300

    fux(DTOs): fix editviewmodel dto

--- ViewModels/EditViewModel.cs
+++ ViewModels/EditViewModel.cs
@@ -4,15 +4,15 @@ namespace Netflix_clone.ViewModels
 {
     public class EditViewModel
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = string.Empty;
 
         [Required]
         [Display(Name = "Username")]
-        public string UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
 
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
     }
 }
