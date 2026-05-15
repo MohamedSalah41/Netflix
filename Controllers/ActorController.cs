43f43d2d7a3e1736d4a4784399fab8bc4f10042b
mohamedkhaledmk <khaled25256@gmail.com>
2026-05-15 09:49:40 +0300

    fix(actor): refactor controller to use generic repo

--- Controllers/ActorController.cs
+++ Controllers/ActorController.cs
@@ -1,54 +1,50 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Netflix_clone.Models;
+using Netflix_clone.Repositories;
 
 namespace Netflix_clone.Controllers
 {
-    //[Authorize(Roles = "Admin")]
     public class ActorController : Controller
     {
-        private readonly NetflixContext _netflixContext;
+        private readonly IGenericRepository<Actor> _actorRepo;
 
-        public ActorController(NetflixContext netflixContext)
+        public ActorController(IGenericRepository<Actor> actorRepo)
         {
-            _netflixContext = netflixContext;
+            _actorRepo = actorRepo;
         }
-        // GET: ActorController
+
         [AllowAnonymous]
         public ActionResult GetActorByName(string name)
         {
-            return View(_netflixContext.Actors.FirstOrDefault(a => a.Name == name));
+            return View(_actorRepo.Find(a => a.Name == name));
         }
 
-        // GET: ActorController/Details/5
         [AllowAnonymous]
         public ActionResult GetActorByID(int id)
         {
-            return View(_netflixContext.Actors.FirstOrDefault(a => a.Id == id));
+            return View(_actorRepo.GetById(id));
         }
 
         [AllowAnonymous]
         public ActionResult GetAllActors()
         {
-            return View(_netflixContext.Actors.ToList());
+            return View(_actorRepo.GetAll());
         }
 
-        // GET: ActorController/Create
         public ActionResult AddActor()
         {
             return View();
         }
 
-        // POST: ActorController/Create
         [HttpPost]
         public ActionResult AddActor(Actor actor)
         {
             try
             {
-                _netflixContext.Actors.Add(actor);
-                _netflixContext.SaveChanges();
+                _actorRepo.Add(actor);
+                _actorRepo.Save();
                 return RedirectToAction(nameof(GetAllActors));
             }
             catch
@@ -57,20 +53,18 @@ namespace Netflix_clone.Controllers
             }
         }
 
-        // GET: ActorController/Edit/5
         public ActionResult UpdateActor(int id)
         {
-            return View(_netflixContext.Actors.FirstOrDefault(a => a.Id == id));
+            return View(_actorRepo.GetById(id));
         }
 
-        // POST: ActorController/Edit/5
         [HttpPost]
         public ActionResult UpdateActor(int id, Actor actor)
         {
             try
             {
-                _netflixContext.Actors.Update(actor);
-                _netflixContext.SaveChanges();
+                _actorRepo.Update(actor);
+                _actorRepo.Save();
                 return RedirectToAction(nameof(GetAllActors));
             }
             catch
@@ -79,21 +73,19 @@ namespace Netflix_clone.Controllers
             }
         }
 
-        // GET: ActorController/Delete/5
         public ActionResult DeleteActor(int id)
         {
-            return View(_netflixContext.Actors.FirstOrDefault(a => a.Id == id));
+            return View(_actorRepo.GetById(id));
         }
 
-        // POST: ActorController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteActor(int id, Actor actor)
         {
             try
             {
-                _netflixContext.Actors.Remove(actor);
-                _netflixContext.SaveChanges();
+                _actorRepo.Delete(actor);
+                _actorRepo.Save();
                 return RedirectToAction(nameof(GetAllActors));
             }
             catch
